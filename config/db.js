e10617e46a96b9eb784118617494147bb6f55c88
brayammor <brayam.moreno.mt@usa.edu.co>
2022-11-24 21:34:24 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const conectarDB = async () => {
+    try{
+        const connection = await mongoose.connect(
+            "mongodb+srv://admin:B2022@cluster0.7epl38g.mongodb.net/?retryWrites=true&w=majority",{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            const url = `${connection.connection.host}:${connection.connection.port}`;
+            console.log(`MongoDB conectado en : ${url}`);
+    }catch (error){
+        console.log(`error: ${error.message}`);
+        process.exit(1);
+    }
+}
+module.exports = conectarDB;
